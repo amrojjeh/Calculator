950b38573403718774449fbb7d7b14e5428049ac
dinglydo <amrojjeh@gmail.com>
2021-02-14 10:57:23 -0600

    Added input support

--- src/main/java/io/github/dinglydo/evaluator/Main.java
+++ src/main/java/io/github/dinglydo/evaluator/Main.java
@@ -2,34 +2,39 @@ package io.github.dinglydo.evaluator;
 
 import io.github.dinglydo.evaluator.expressions.Distributor;
 import io.github.dinglydo.evaluator.expressions.Polynomial;
+import io.github.dinglydo.evaluator.lexer.Lexer;
+import io.github.dinglydo.evaluator.parser.Parser;
 import io.github.dinglydo.evaluator.primitive.Term;
+
+import java.text.ParseException;
+import java.util.Scanner;
 
 public class Main
 {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        Term t1 = new Term(0);
-        Term t2 = new Term(5);
-        Term t3 = new Term(10);
-        Term t4 = new Term(5).negate();
-        Term t5 = new Term(5, 'x').multiply('x');
-        Term t6 = new Term(10, 'x');
-        Term t7 = new Term(25, 'y', 'x');
-        Term t8 = new Term(3).multiply(new Term(3, 'x')).multiply(new Term(3, 'y', 'z'));
+        boolean running = true;
+        while (running)
+        {
+            String input = prompt().trim();
+            if (input.equals("q") || input.equals("equals"))
+                running = false;
+            else {
+                try {
+                    Polynomial p = Parser.parse(Lexer.lex(input)).getPolynomial();
+                    System.out.println(p.simplify());
+                } catch (ParseException e) {
+                    System.out.println("Couldn't parse input. " + e.toString());
+                }
+            }
+        }
+    }
 
-        Polynomial p = new Polynomial(t1, t2, t3, t4, t5, t6, t7, t8);
-
-        System.out.println(p.toString());
-
-        System.out.println("Time to simplify!");
-
-        p = p.simplify();
-
-        System.out.println(p.toString());
-
-        System.out.println("Now let's distribute 2x!");
-
-        p = new Distributor(p, new Term(2, 'x')).simplify();
-        System.out.println(p.toString());
+    public static String prompt()
+    {
+        System.out.print(">> ");
+        return scanner.nextLine();
     }
 }
